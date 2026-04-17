71c4462981545b8e829af2b62164553ef517bd55
Jeroen Kloppenburg <jeroen.kloppenburg@xs4all.nl>
2026-04-17 20:57:31 +0200

    Hello devices

---
+++ MVP/Program.cs
@@ -0,0 +1,21 @@
+﻿using SharpPcap;
+using SharpPcap.LibPcap;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var devices = CaptureDeviceList.Instance;
+
+        if (devices.Count == 0)
+        {
+            Console.WriteLine("No devices found");
+            return;
+        }
+
+        foreach(var device in devices)
+        {
+            Console.WriteLine($"{device.Name} - {device.Description}");
+        }
+    }
+}
